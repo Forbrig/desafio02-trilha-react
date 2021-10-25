1e20288aa733dea2433aa4b0d4ca238c0956b2ff
Vitor <vitorforbrig@gmail.com>
2021-10-25 11:47:55 -0300

    Componentized Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,46 @@
-export function Content() {
+import { MovieCard } from "./MovieCard";
+
+import "../styles/content.scss";
+
+interface ContentProps {
+  title: string;
+  movies: MovieProps[];
+}
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function Content({ title, movies }: ContentProps) {
   // Complete aqui
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }

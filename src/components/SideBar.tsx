9eacbf8d230788e367140369a912a5ac617ac040
Vitor <vitorforbrig@gmail.com>
2021-10-25 12:13:30 -0300

    Componentized SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,41 @@
-export function SideBar() {
+import "../styles/sidebar.scss";
+
+import { Button } from "../components/Button";
+
+interface SideBarProps {
+  onChangeGenre: (id: number) => void;
+  selectedGenreId: number;
+  genres: GenreProps[];
+}
+interface GenreProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+export function SideBar({
+  genres,
+  onChangeGenre,
+  selectedGenreId,
+}: SideBarProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onChangeGenre(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
